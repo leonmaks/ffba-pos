c588319bb214e31e5e81a7a1f205fdf0e51a5516
Leon <le_ma_@mail.ru>
2023-04-08 22:08:52 +0300

    Delete admin/pg-wsp directory

--- admin/pg-wsp/elta_00003/pos.sql
+++
@@ -1,8 +0,0 @@
-DOWN
-  elta_00003
-  id= 3
-  siteguid= 7e1b1f6a-e7f8-4e7d-acf4-8a25604c0b3c
-;
-
-select id, siteguid from ffba_pos where ident = 'elta_00003'
-;
--- admin/pg-wsp/pos_source.sql
+++
@@ -1,4 +0,0 @@
-eltapos_003
-id= 5
-siteguid = '34892ae3-3656-4601-92a3-16ba73b14dcb'
-
